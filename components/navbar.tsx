736630e7f3e55661c814aee4c5ad7a000f3c15b3
Edwin Shen <edshen17@avenues.org>
2022-06-14 15:35:56 -0400

    finish navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,33 +1,42 @@
 import Link from "next/link";
 import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const toggleIcon = () => {
+    setIsOpen(!isOpen);
+  }
+
+  const openMailer = () => {
+    window.location.href = 'mailto:christian@tripleaxle.com?subject=Hi Christian!'
+  }
 
   const routerLinks = [
     {
       title: 'About',
       path: '/about',
+      action: toggleIcon,
     },
     {
       title: 'Dogs',
       path: '/dogs',
+      action: toggleIcon,
     },
     {
       title: 'Contact Us',
-      path: '/contact-us',
+      path: '#',
+      action: openMailer,
     },
   ]
-
+  
   const routerList = routerLinks.map((routerLink) => 
     <Link href={routerLink.path} key={routerLink.path}>
-      <a className="py-3 sm:mt-0 sm:ml-2 block sm:py-0 text-gray-500 hover:text-gray-800 px-4">{routerLink.title}</a>
+      <a className="py-3 sm:mt-0 sm:ml-2 block sm:py-0 text-gray-500 hover:text-gray-800 px-4"  onClick={routerLink.action}>{routerLink.title}</a>
     </Link>
   )
-
-  const onClick = () => {
-    setIsOpen(!isOpen);
-  }
 
   return <header className="shadow-md sm:flex sm:justify-between sm:px-4 sm:py-4 sm:items-center">
     <div className="flex items-center justify-between px-4 py-5 sm:p-0">
@@ -37,8 +46,8 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="sm:hidden">
-        <button type="button" className="block" onClick={onClick} >
-          Menu Button
+        <button type="button" className="block" onClick={toggleIcon} >
+          <FontAwesomeIcon icon={isOpen ? faX : faBars } className="text-xl px-2" />
         </button>
       </div>
     </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import DefaultLayout from '../layouts/default-layout'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <DefaultLayout>
